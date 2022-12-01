b0675583ecbea7a2609cdb4dd65022fcdcf276f6
44Chick <to44guchi@gmail.com>
2022-12-01 10:41:40 +0900

    항해99 React Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [detail, setDetail] = useState('');
+
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const newTodos = {
+      id: new Date().getTime(),
+      detail: detail
+    }
+    setTodo([...todo, newTodos])
+    setDetail('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addTodo}>
+        <input
+          type='text'
+          required
+          value={detail}
+          onChange={(e) => setDetail(e.target.value)}
+        ></input>
+        <button>추가하기</button>
+      </form>
+      <div>
+        <h2>ToDo List</h2>
+        <div className='todo-list'>
+          {todo.map(v => {
+            return (
+              <div className='todo-cards' key={v.id}>{v.detail}</div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
